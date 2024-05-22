e1a146883435d504e7ad9912d993d8347f6c160c
nasyasakhi <nasyasakhi6@gmail.com>
2024-05-22 09:20:26 +0700

    mencoba push

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,8 @@
 
 </head>
 <body>
+
+<!-- ini percobaan  -->
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
